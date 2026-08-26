a97888f49ee88838e2c59d70556b7af374e7e1b5
Jdub <jwotton91@gmail.com>
2026-08-26 11:31:53 -0400

    Initial docs with relay dashboard

---
+++ docs/conf.py
@@ -0,0 +1,6 @@
+project = 'Service Tools'
+copyright = '2024'
+author = 'Team'
+extensions = []
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,7 @@
+Service Tools
+=============
+
+Documentation for internal service tools.
+
+.. toctree::
+   :maxdepth: 2
